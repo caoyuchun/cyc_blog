7cd722707167dc545aa03fbfd570d542033c9453
caoyuchun <523481343@qq.com>
2021-01-05 14:56:41 +0800

    t

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 
 /** 载入配置支持 */
 if (!defined('__TYPECHO_ROOT_DIR__') && !@include_once 'config.inc.php') {
-   // file_exists('./install.php') ? header('Location: install.php') : print('Missing Config File');
+    file_exists('./install.php') ? header('Location: install.php') : print('Missing Config File');
     exit;
 }
 
